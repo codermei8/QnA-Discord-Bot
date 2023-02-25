a204cb262102f3fa43d996bbd9e85abfed740151
codermei8 <119540082+codermei8@users.noreply.github.com>
2023-02-25 15:22:04 -0800

    hello bot v1
    The bot is currently capable of handling conditional logic such that in response to messages, it can parse and respond accordingly.

---
+++ hello.py
@@ -0,0 +1,25 @@
+import discord
+
+#Specify what type of access the bot will have. In our case, all.
+intents = discord.Intents.all()
+intents.members = True
+
+#Creates a new discord client
+client = discord.Client(intents=intents)
+
+#Event handler that prints "Logged in" when the discord bot is up and running.
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+#Event handler that receives a message and replies "hello" to the user. 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.lower() == "hello" or "hi":
+        await message.channel.send(f"Hello {message.author.name}!")
+    else:
+        await message.channel.send("Hello!")
+
+client.run('MTA3MzgxODY5NzAxNTI1MDk4NA.G_xB2Q.KA1jHi8617RUHQSv6R6sOTWCHLL66sRTc0xMbA')
